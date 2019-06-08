c9c01d70b0556923d315c15c229b9f445016c7e5
Erre96 <erre_erhan@hotmail.com>
2019-06-08 16:34:18 +0200

    update chapter page

---
+++ lib/screens/chapter_screen/audio_player.dart
@@ -0,0 +1,79 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class MyAudioPlayer extends StatefulWidget {
+  @override
+  _MyAudioPlayer createState() => new _MyAudioPlayer();
+}
+
+class _MyAudioPlayer extends State<MyAudioPlayer> {
+
+  static AudioCache advancedPlayer = new AudioCache(prefix: 'tracks/');
+  static AudioPlayer audioPlayer = new AudioPlayer();
+  bool playing = false;
+  double sliderValue = 0;
+
+  @override
+  initState() {
+    super.initState();
+  }
+
+  Future loadMusic() async {
+    //advancedPlayer = await AudioCache().loop("assets/tracks/gg.mp3");
+  }
+    playLocal() async {
+      advancedPlayer.play('gg.mp3');
+    }
+  
+  
+    playRemote() async {
+      getTrackPosition();
+      if(playing == false)
+      {
+        await audioPlayer.play('https://api.soundcloud.com/tracks/434370309/stream?secret_token=s-tj3IS&client_id=LBCcHmRB8XSStWL6wKH2HPACspQlXg2P');
+        setState(() {
+          playing = true;
+        });
+      }
+      else
+      if(playing == true)
+      {
+          await audioPlayer.pause();
+                  setState(() {
+          playing = false;
+        });
+      }
+  }
+
+ getTrackPosition()
+{
+  audioPlayer.onAudioPositionChanged.listen((Duration  p) {
+    print('Current position: $p');
+    return p;
+  });
+}
+
+  @override
+  void dispose() {
+    audioPlayer = null;
+    super.dispose();
+  }
+
+ @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 55),
+      child: new Row(
+        children: <Widget>[
+          new RaisedButton(
+            child: playing ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+            onPressed: () {
+                playRemote();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/chapter_screen/chapter_screen.dart
+++ lib/screens/chapter_screen/chapter_screen.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:karlekstanken/models/chapter.dart';
 import 'package:karlekstanken/db_service_provider.dart';
+import 'package:karlekstanken/my_colors.dart';
+import 'video_player_my.dart';
 
 class ChapterScreen extends StatefulWidget {
   ChapterScreen(this._chapter);
@@ -32,11 +34,14 @@ class _ChapterScreenState extends State<ChapterScreen> {
     List<Task> tasks;
     FirebaseUser user = await FirebaseAuth.instance.currentUser();
     if (user != null) {
-      tasks = await DatabaseServiceProvider.of(context).db.getTasks(_chapter.id);
+      tasks =
+          await DatabaseServiceProvider.of(context).db.getTasks(_chapter.id);
     } else {
-      tasks = await DatabaseServiceProvider.of(context).db.getTasksUnauthenticated(_chapter.id);
+      tasks = await DatabaseServiceProvider.of(context)
+          .db
+          .getTasksUnauthenticated(_chapter.id);
     }
-     
+
     setState(() {
       _tasks = tasks;
     });
@@ -51,6 +56,7 @@ class _ChapterScreenState extends State<ChapterScreen> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          new VideoPlayerScreen(),
           Text(_chapter.video),
           Text(_chapter.text),
           Expanded(
@@ -59,13 +65,71 @@ class _ChapterScreenState extends State<ChapterScreen> {
                 : ListView.builder(
                     itemCount: _tasks.length,
                     itemBuilder: (context, pos) {
-                      return ListTile(
-                        title: Text(_tasks[pos].title),
+                      return GestureDetector(
+                        onTap: () {},
+                        child: ExerciseBox(_tasks[pos].title, false),
                       );
                     },
                   ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class ExerciseBox extends StatelessWidget {
+  final String title;
+  final bool complete;
+
+  ExerciseBox(this.title, this.complete);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 5),
+        color: MyColors.secondary,
+        child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              new Container(
+                alignment: Alignment.center,
+                child: Container(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new Container(
+                          padding: EdgeInsets.fromLTRB(45, 15, 50, 15),
+                          child: Text(
+                            title,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        complete
+                            ? new Container(
+                                padding: EdgeInsets.fromLTRB(45, 0, 50, 0),
+                                child: Text(
+                                  'Färdig',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green[500],
+                                  ),
+                                ),
+                              )
+                            : SizedBox(),
+                      ]),
+                ),
+              ),
+            ]),
       ),
     );
   }
