0aed436291c36f1c0f05a5a3b51faf6a4d1e9402
Abed1230 <abedfetrat@outlook.com>
2019-08-08 13:55:08 +0200

    change heart shape

--- lib/screens/home_screen/widgets/my_progress_indicator.dart
+++ lib/screens/home_screen/widgets/my_progress_indicator.dart
@@ -2,44 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
 class MyProgressIndicator extends StatelessWidget {
-
   MyProgressIndicator(this._totalTasks, this._completedTasks);
 
-  final int _totalTasks; 
+  final int _totalTasks;
   final int _completedTasks;
 
   double _calculateValue() {
-    return (_completedTasks/_totalTasks);
+    return (_completedTasks / _totalTasks);
   }
 
   Path _buildHeartPath() {
     return new Path()
-     ..moveTo(44, 12)
-      ..cubicTo(44, 9.6, 40, 0, 24, 0)
-      ..cubicTo(0, 0, 0, 30, 0, 30)
-      ..cubicTo(0, 48, 16, 61.6, 44, 80)
-      ..cubicTo(72, 61.6, 88, 48, 88, 30)
-      ..cubicTo(88, 30, 88, 0, 64, 0)
-      ..cubicTo(52, 0, 44, 9.6, 44, 12)
+      ..moveTo(42, 15.522499999999999)
+      ..cubicTo(
+          35.0385, -3.3740000000000014, 0, -0.5670000000000028, 0, 28.0105)
+      ..cubicTo(0, 42.2485, 10.71, 61.194, 42, 80.5)
+      ..cubicTo(73.28999999999999, 61.194, 84, 42.2485, 84, 28.0105)
+      ..cubicTo(84, -0.40250000000000075, 49, -3.4860000000000015, 42, 15.5225)
       ..close();
   }
 
   @override
   Widget build(BuildContext context) {
     return LiquidCustomProgressIndicator(
-        value: _calculateValue(),
-        direction: Axis.vertical,
-        backgroundColor: Colors.red.withAlpha(150),
-        valueColor: AlwaysStoppedAnimation(Colors.red),
-        shapePath: _buildHeartPath(),
-        center: Text(
-          "${(_calculateValue() * 100).toStringAsFixed(0)}%",
-          style: TextStyle(
-            color: Colors.red[800],
-            fontSize: 16.0,
-            fontWeight: FontWeight.bold,
-          ),
+      value: _calculateValue(),
+      direction: Axis.vertical,
+      backgroundColor: Colors.red.withAlpha(150),
+      valueColor: AlwaysStoppedAnimation(Colors.red),
+      shapePath: _buildHeartPath(),
+      center: Text(
+        "${(_calculateValue() * 100).toStringAsFixed(0)}%",
+        style: TextStyle(
+          color: Colors.red[800],
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
         ),
-      );
+      ),
+    );
   }
 }
